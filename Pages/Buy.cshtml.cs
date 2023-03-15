ddaa9ed9f1968ab54dd36caa92730371f70d91f3
Chad Yorgason <chad.yorgason@gmail.com>
2023-03-15 13:28:50 -0600

    Mission11Final

--- Pages/Buy.cshtml.cs
+++ Pages/Buy.cshtml.cs
@@ -12,31 +12,34 @@ namespace Bookstore.Pages
     public class BuyModel : PageModel
     {
         private IBookstoreRepository repo { get; set; }
-
-        public BuyModel (IBookstoreRepository temp)
-        {
-            repo = temp;
-        }
-
         public Cart cart { get; set; }
         public string ReturnUrl { get; set; }
+
+        public BuyModel (IBookstoreRepository temp, Cart c)
+        {
+            repo = temp;
+            cart = c;
+        }
 
         public void OnGet(string returnUrl)
         {
             ReturnUrl = returnUrl ?? "/";
-            cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
         }
 
         public IActionResult OnPost(int bookId, string returnUrl)
         {
             Book b = repo.Books.FirstOrDefault(x => x.BookId == bookId);
 
-            cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
             cart.AddItem(b, 1);
 
-            HttpContext.Session.SetJson("cart", cart);
-
             return RedirectToPage(new { ReturnUrl = returnUrl});
+        }
+
+        public IActionResult OnPostRemove(int bookId, string returnUrl)
+        {
+            cart.RemoveItem(cart.Items.First(x => x.Book.BookId == bookId).Book);
+
+            return RedirectToPage(new { ReturnUrl = returnUrl });
         }
     }
 }
